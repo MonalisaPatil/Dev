65defc39f77e8e4f656ea0ff2b8d71b98bc0d5e6
Monalisa Patil <90000984+MonalisaPatil@users.noreply.github.com>
2025-01-08 13:33:47 -0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,13 @@ public class Main {
         System.out.println("Hello Mona!");
     }
 }
+
+//PDT:: Integral-->> Byte1 Short2 Int4 Long8
+//      FloatingPoint-->>Float4 Double8
+//      Char2
+//      Boolean
+
+//Variable:: (data type) variable Name = value
+//            --> Contains --> Alphabet , Numbers , _ , $ (allowed)
+//            --> Initiated With --> Alphabet,_, $
+//            --> Camel Cases, no limit on length of name
