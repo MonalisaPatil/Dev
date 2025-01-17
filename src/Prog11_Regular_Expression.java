fa1c9124fea7b111a895588d1ff29f76f5429e15
Monalisa Patil <90000984+MonalisaPatil@users.noreply.github.com>
2025-01-16 19:53:57 -0600

    Update Jan17/01

--- src/Prog11_Regular_Expression.java
+++ src/Prog11_Regular_Expression.java
@@ -3,8 +3,19 @@ public class Prog11_Regular_Expression {
 
         String str1="mp";
         System.out.println(str1.matches("."));
-        String st2="A";
-        System.out.println(st2.matches("[a-zA-Z]"));
+        String str2="A";
+        System.out.println(str2.matches("[a-zA-Z]"));
+        String str3= "M";
+        System.out.println(str3.matches("S|M"));
+
+        //Quantifier
+
+        String str4 = "SJ MP";
+        System.out.println(str4.matches(".*"));
+        String str5 = "SJ0724MP";
+        System.out.println(str5.matches("[A-Z][0-9]*"));
+
+
 
     }
 }
@@ -17,3 +28,11 @@ public class Prog11_Regular_Expression {
 //     [a-z1-7]         a-z or 1-7
 //       A|B            A or B
 //       XZ             Exactly XZ
+
+//           QUANTIFIERS
+//Regular Expression:: Description
+//         *           0 or more time
+//         +           one or more
+//         ?           0 or 1 time
+//        {X}          X times
+//       {X,Y}         Between X and Y time
